97d66a20a18fd39638ae0d085e7bca9f4c1925f8
Andrey Gubanov <andrey.a.gubanov@gmail.com>
2016-05-11 18:53:41 +0300

    add function call test

---
+++ test/fixtures/function-call/expected.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var _keys,
+    _l,
+    _i,
+    _source,
+    _key,
+    _result = {};
+
+_result.a = a
+
+for (_source = b, _keys = Object.keys(_source), _l = _keys.length, _i = 0; _i < _l; _i++) {
+  _key = _keys[_i];
+  _result[_key] = _source[_key];
+}
+
+_result.c = c
+f(_result);
